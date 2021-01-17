c0db47bf71b19b9f250f2c54794cf83fb6f57efe
Radomir Radionov <66359081+radomir-radionov@users.noreply.github.com>
2021-01-17 23:46:14 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1459,3 +1459,23 @@
 // const arr = [NaN];
 // alert( arr.indexOf(NaN) ); // -1 (должен быть 0, но === проверка на равенство не работает для NaN)
 // alert( arr.includes(NaN) );// true (верно)
+
+// Поиск в массиве
+// Далее рассмотрим методы, которые помогут найти что-нибудь в массиве.
+
+// indexOf/lastIndexOf и includes
+// Методы arr.indexOf, arr.lastIndexOf и arr.includes имеют одинаковый синтаксис и делают по сути то же самое,
+// что и их строковые аналоги, но работают с элементами вместо символов:
+
+// arr.indexOf(item, from) ищет item, начиная с индекса from, и возвращает индекс, на котором был найден искомый элемент, в противном случае -1.
+// arr.lastIndexOf(item, from) – то же самое, но ищет справа налево.
+// arr.includes(item, from) – ищет item, начиная с индекса from, и возвращает true, если поиск успешен.
+// Например:
+
+// let arr = [1, 0, false];
+
+// alert( arr.indexOf(0) ); // 1
+// alert( arr.indexOf(false) ); // 2
+// alert( arr.indexOf(null) ); // -1
+
+// alert( arr.includes(1) ); // true
